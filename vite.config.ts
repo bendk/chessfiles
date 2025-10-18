b635a36954c0c4531e5de790e2a6bc3ed6000282
Ben Dean-Kawamura <bdk@mozilla.com>
2025-10-18 12:30:53 -0400

    Github deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import solid from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  // Set base based on the github static site (https://bendk.github.io/chessfiles/)
+  base: "/chessfiles/",
   plugins: [solid(), tailwindcss()],
   resolve: {
     alias: {
