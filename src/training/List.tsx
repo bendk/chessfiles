04e1cdb730f4a9c5398d65d7b5880da5fe7ebc7e
Ben Dean-Kawamura <bdk@mozilla.com>
2025-11-28 11:16:30 -0500

    Training page settings

--- src/training/List.tsx
+++ src/training/List.tsx
@@ -1,12 +1,22 @@
 import Plus from "lucide-solid/icons/plus";
 import Loader from "lucide-solid/icons/loader-2";
-import { Match, Show, Switch, createResource, createSignal } from "solid-js";
+import IconSettings from "lucide-solid/icons/settings";
+import {
+  Match,
+  Show,
+  Switch,
+  createEffect,
+  createResource,
+  createSignal,
+} from "solid-js";
 import type { MenuItem, Page, StatusTracker } from "~/components";
 import {
   Button,
+  Checkbox,
   Chooser,
   Dialog,
   Progress,
+  RadioGroup,
   StandardLayout,
   StatusError,
   Table,
@@ -14,7 +24,7 @@ import {
   TableMenuCell,
 } from "~/components";
 import type { AppStorage } from "~/lib/storage";
-import type { TrainingMeta } from "~/lib/training";
+import type { TrainingMeta, TrainingSettings } from "~/lib/training";
 import { trainingTimeAgo } from "~/lib/training";
 import { FileNotFoundError, TrainingExistsError } from "~/lib/storage";
 
@@ -25,21 +35,26 @@ export interface TrainingListProps {
   setPage: (page: Page) => void;
 }
 
+type DialogInfo =
+  | { type: "finished"; meta: TrainingMeta }
+  | { type: "settings" };
+
 export function TrainingList(props: TrainingListProps) {
   const [showChooser, setShowChooser] = createSignal(false);
   const [chooserError, setChooserError] = createSignal<string | undefined>();
+  const [trainingSettings, setTrainingSettings] =
+    createSignal<TrainingSettings | null>(null);
+  const [dialog, setDialog] = createSignal<DialogInfo | null>(null);
 
-  const [finishedMetaDialog, setFinishedMetaDialog] =
-    createSignal<TrainingMeta | null>(null);
   async function restartFininshedMeta() {
-    const meta = finishedMetaDialog();
-    if (meta === null) {
-      console.log("restartFininshedMeta with null meta");
+    const d = dialog();
+    if (d === null || d.type != "finished") {
+      console.error("invalid dialog for restartFininshedMeta (${d})");
       return;
     }
-    await onMenuAction(meta, "restart");
-    setFinishedMetaDialog(null);
-    await onMenuAction(meta, "open");
+    await onMenuAction(d.meta, "restart");
+    setDialog(null);
+    await onMenuAction(d.meta, "open");
   }
 
   const [trainingListing, { refetch: refetchTrainingListing }] = createResource(
@@ -67,7 +82,7 @@ export function TrainingList(props: TrainingListProps) {
   async function onMenuAction(meta: TrainingMeta, action: string) {
     if (action == "open") {
       if (meta.linesTrained >= meta.totalLines) {
-        setFinishedMetaDialog(meta);
+        setDialog({ type: "finished", meta });
         return;
       }
       props.status.perform("opening training", async () => {
@@ -96,6 +111,58 @@ export function TrainingList(props: TrainingListProps) {
   }
 
   const currentTimestamp = Date.now();
+
+  createEffect(async () => {
+    if (trainingSettings() === null) {
+      setTrainingSettings(await props.storage.readTrainingSettings());
+    }
+  });
+
+  function updateTrainingSettings(settings: TrainingSettings) {
+    props.storage.saveTrainingSettings(settings);
+    setTrainingSettings(settings);
+  }
+
+  function updateTrainingShuffle(shuffle: boolean | null) {
+    const current = trainingSettings();
+    if (shuffle === null || current === null) {
+      return;
+    }
+    updateTrainingSettings({
+      ...current,
+      shuffle,
+    });
+  }
+
+  function updateTrainingRetrainLimit(value: string | null) {
+    const current = trainingSettings();
+    if (value === null || current === null) {
+      return;
+    }
+    const skipAfter = Number.parseInt(value, 10);
+    if (isNaN(skipAfter)) {
+      return;
+    }
+    updateTrainingSettings({
+      ...current,
+      skipAfter,
+    });
+  }
+
+  function updateTrainingMoveDelay(value: string | null) {
+    const current = trainingSettings();
+    if (value === null || current === null) {
+      return;
+    }
+    const moveDelay = Number.parseFloat(value);
+    if (isNaN(moveDelay)) {
+      return;
+    }
+    updateTrainingSettings({
+      ...current,
+      moveDelay,
+    });
+  }
 
   return (
     <>
@@ -132,17 +199,64 @@ export function TrainingList(props: TrainingListProps) {
         </Match>
         <Match when={true}>
           <StandardLayout page="training" setPage={props.setPage}>
-            <Show when={finishedMetaDialog()}>
-              <Dialog
-                onSubmit={restartFininshedMeta}
-                onClose={() => setFinishedMetaDialog(null)}
-                title="Training is finished"
-                submitText="Restart Training"
-              >
-                <div>You've already finished this training</div>
-              </Dialog>
-            </Show>
             <Switch>
+              <Match when={dialog()?.type == "finished"}>
+                <Dialog
+                  onSubmit={restartFininshedMeta}
+                  onClose={() => setDialog(null)}
+                  title="Training is finished"
+                  submitText="Restart Training"
+                >
+                  <div>You've already finished this training</div>
+                </Dialog>
+              </Match>
+              <Match when={dialog()?.type == "settings"}>
+                <Dialog
+                  onClose={() => setDialog(null)}
+                  closeText="Close"
+                  title="Training Settings"
+                >
+                  <Show when={trainingSettings()} keyed>
+                    {(settings) => (
+                      <div class="flex flex-col gap-4">
+                        <Checkbox
+                          label="Randomize lines"
+                          checked={settings.shuffle}
+                          onChange={updateTrainingShuffle}
+                        />
+
+                        <RadioGroup.Root
+                          value={settings.skipAfter.toString()}
+                          onValueChange={updateTrainingRetrainLimit}
+                        >
+                          <RadioGroup.Label
+                            text="Retrain limit"
+                            help="Skip moves after getting them correct this many times in a row"
+                          />
+                          <RadioGroup.Item text="2 moves" value="2" />
+                          <RadioGroup.Item text="3 moves" value="3" />
+                          <RadioGroup.Item text="4 moves" value="4" />
+                          <RadioGroup.Item
+                            text="Always retrain moves"
+                            value="0"
+                          />
+                        </RadioGroup.Root>
+
+                        <RadioGroup.Root
+                          value={settings.moveDelay.toString()}
+                          onValueChange={updateTrainingMoveDelay}
+                        >
+                          <RadioGroup.Label text="Opponent Move Delay" />
+                          <RadioGroup.Item text="None" value="0" />
+                          <RadioGroup.Item text="0.25 seconds" value="0.25" />
+                          <RadioGroup.Item text="0.5 seconds" value="0.5" />
+                          <RadioGroup.Item text="1 second" value="1" />
+                        </RadioGroup.Root>
+                      </div>
+                    )}
+                  </Show>
+                </Dialog>
+              </Match>
               <Match when={trainingListing.loading}>
                 <Loader class="animate-spin duration-1000" size={32} />
               </Match>
@@ -197,13 +311,6 @@ export function TrainingList(props: TrainingListProps) {
                         </Table>
                       </Match>
                     </Switch>
-                    <div class="flex pt-8">
-                      <Button
-                        text="New Training Session"
-                        icon=<Plus />
-                        onClick={() => setShowChooser(true)}
-                      />
-                    </div>
                   </div>
                   <div class="flex-1">
                     <Show when={trainingListing()?.activity}>
@@ -248,6 +355,18 @@ export function TrainingList(props: TrainingListProps) {
                       </Table>
                     </Show>
                   </div>
+                </div>
+                <div class="flex pt-16 gap-8">
+                  <Button
+                    text="Session"
+                    icon=<Plus />
+                    onClick={() => setShowChooser(true)}
+                  />
+                  <Button
+                    text="Settings"
+                    icon=<IconSettings />
+                    onClick={() => setDialog({ type: "settings" })}
+                  />
                 </div>
               </Match>
             </Switch>
